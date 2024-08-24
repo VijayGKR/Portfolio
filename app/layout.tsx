42c136c7b5b37bc805e28c0f1eee84709d205cb7
VijayGKR <vkumarav@usc.edu>
2024-08-24 10:40:23 -0700

    Favicon and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Vijay's Portfolio",
   description: "Vijay Kumaravelrajan's portfolio website with projects and blog posts",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className }>{children}</body>
+      <body className={inter.className }>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
