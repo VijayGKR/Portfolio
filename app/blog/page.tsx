f486bf157fdc6c030f6c0399d7a1447b2b3d6fb3
VijayGKR <vkumarav@usc.edu>
2024-08-24 00:08:33 -0700

    Fixed nav bar for blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,26 +2,26 @@ import Link from 'next/link';
 
 export default function Blog() {
   return (
-    <main className="min-h-screen bg-black text-white font-serif p-2 xs:p-4 sm:p-8">
-      <nav className="fixed left-0 top-0 sm:top-1/2 sm:-translate-y-1/2 w-full sm:w-auto sm:h-auto flex sm:flex-col justify-center items-center sm:items-start z-10 bg-black bg-opacity-70 sm:bg-opacity-0">
-        <ul className="flex sm:flex-col space-x-2 xs:space-x-4 sm:space-x-0 sm:space-y-8 p-2 xs:p-4 sm:p-6">
+    <main className="min-h-screen bg-black text-white font-serif p-4 sm:p-8">
+       <nav className="fixed left-0 top-0 sm:top-1/2 sm:-translate-y-1/2 w-full sm:w-auto sm:h-auto flex sm:flex-col justify-center items-center sm:items-start z-10 bg-black bg-opacity-70 sm:bg-opacity-0">
+        <ul className="flex sm:flex-col space-x-4 sm:space-x-0 sm:space-y-8 p-4 sm:p-6">
           <li>
-            <Link href="/" className="text-white hover:text-gray-300 transition duration-300 text-xs xs:text-sm sm:text-lg">
+            <Link href="/" className="text-white hover:text-gray-300 transition duration-300 text-sm sm:text-lg">
               Home
             </Link>
           </li>
           <li>
-            <Link href="/about" className="text-white hover:text-gray-300 transition duration-300 text-xs xs:text-sm sm:text-lg">
+            <Link href="/about" className="text-white hover:text-gray-300 transition duration-300 text-sm sm:text-lg">
               About
             </Link>
           </li>
           <li>
-            <Link href="/projects" className="text-white hover:text-gray-300 transition duration-300 text-xs xs:text-sm sm:text-lg">
+            <Link href="/projects" className="text-white hover:text-gray-300 transition duration-300 text-sm sm:text-lg">
               Projects
             </Link>
           </li>
           <li>
-            <Link href="/blog" className="text-white hover:text-gray-300 transition duration-300 text-xs xs:text-sm sm:text-lg">
+            <Link href="/blog" className="text-white hover:text-gray-300 transition duration-300 text-sm sm:text-lg">
               Blog
             </Link>
           </li>
